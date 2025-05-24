7dc281bb98106e88f75e21a61bfc060f2994ea5a
paiad <2369538173@qq.com>
2025-05-24 12:18:53 +0800

    docs: update logo

--- src/views/_builtin/login/index.vue
+++ src/views/_builtin/login/index.vue
@@ -54,7 +54,7 @@ const activeModule = computed(() => moduleMap[props.module || 'pwd-login']);
     <ElCard class="relative z-4 w-auto rd-12px">
       <div class="w-400px lt-sm:w-300px">
         <header class="flex-y-center justify-between">
-          <SystemLogo class="text-64px text-primary lt-sm:text-48px" />
+          <SystemLogo class="text-50px text-primary lt-sm:text-48px" />
           <h3 class="text-28px text-primary font-500 lt-sm:text-22px">{{ $t('system.title') }}</h3>
           <div class="i-flex-col">
             <ThemeSchemaSwitch
