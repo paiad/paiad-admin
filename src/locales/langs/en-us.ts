e99d1f4334fe35ba96ab5843c552a9ac57bba280
paiad <2369538173@qq.com>
2025-05-28 00:55:58 +0800

    docs(login): update index.vue

--- src/locales/langs/en-us.ts
+++ src/locales/langs/en-us.ts
@@ -280,7 +280,7 @@ const local: App.I18n.Schema = {
     },
     about: {
       title: 'About',
-      introduction: `SoybeanAdmin is an elegant and powerful admin template, based on the latest front-end technology stack, including Vue3, Vite5, TypeScript, Pinia and UnoCSS. It has built-in rich theme configuration and components, strict code specifications, and an automated file routing system. In addition, it also uses the online mock data solution based on ApiFox. SoybeanAdmin provides you with a one-stop admin solution, no additional configuration, and out of the box. It is also a best practice for learning cutting-edge technologies quickly.`,
+      introduction: `paiad-admin provides you with an all-in-one backend management solution that works out of the box with no additional configuration required. It's also a best practice for quickly learning cutting-edge technologies.`,
       projectInfo: {
         title: 'Project Info',
         version: 'Version',
--- src/views/_builtin/login/index.vue
+++ src/views/_builtin/login/index.vue
@@ -4,12 +4,12 @@ import type { Component } from 'vue';
 import { loginModuleRecord } from '@/constants/app';
 import { useAppStore } from '@/store/modules/app';
 import { useThemeStore } from '@/store/modules/theme';
+import { $t } from '@/locales';
 import PwdLogin from './modules/pwd-login.vue';
 import CodeLogin from './modules/code-login.vue';
 import Register from './modules/register.vue';
 import ResetPwd from './modules/reset-pwd.vue';
 import BindWechat from './modules/bind-wechat.vue';
-import {$t} from "../../../locales";
 
 defineOptions({ name: 'LoginPage' });
 
@@ -54,7 +54,7 @@ const activeModule = computed(() => moduleMap[props.module || 'pwd-login']);
         <header class="w-full flex items-center justify-between pt-5 pb-3">
           <div class="w-1/10"></div>
           <SystemLogo class="text-40px text-primary lt-sm:text-48px" />
-          <h2 class="select-none text-2xl font-extrabold tracking-wide">{{ $t('page.login.common.welcomeLogin') }}</h2>
+          <h2 class="select-none text-2xl font-extrabold tracking-wide pl-3">{{ $t('page.login.common.welcomeLogin') }}</h2>
           <div class="i-flex-row">
             <LangSwitch
               v-if="themeStore.header.multilingual.visible"
